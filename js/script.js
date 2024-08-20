d9b3d35b299e3eefb5fe9626da7a4b666ac158a9
lina marin <linamarin2011@gmail.com>
2024-08-20 10:32:09 -0500

    se realizan primeros dos puntos

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+/* Ejercicio 1: Comparar Edad
+1. Solicita al usuario que ingrese su edad.
+2. Si la edad es mayor o igual a 18, muestra un mensaje que diga "Eres mayor de edad".
+3. Si no, muestra "Eres menor de edad". */
+
+
+
+ /* let edad = prompt("Ingrese su edad: ");
+
+
+if(edad>=18){
+    console.log("Eres mayor de edad")
+}
+
+else{
+    console.log("Eres menor de edad")
+
+} */
+
+
+
+
+  /*   Ejercicio 2: Número Positivo o Negativo
+1. Solicita al usuario que ingrese un número.
+2. Si el número es mayor o igual a 0, muestra "El número es positivo".
+3. Si no, muestra "El número es negativo". */
+
+
+
+/* let numero = prompt("Ingresa un número positivo o negativo: ");
+
+if(numero >=0){
+    console.log("El número es positivo")
+}
+else{
+    console.log("El número es negativo")
+} */
+
+
+    /* Ejercicio 3: Par o Impar
+    1. Solicita al usuario que ingrese un número.
+    2. Usa el operador módulo (%) para verificar si el número es par o impar.
+    3. Muestra por consola "El número es par" o "El número es impar" según el resultado. */
+
+    
+
